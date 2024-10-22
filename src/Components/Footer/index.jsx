9b5058bca59e6ff52484421c6e7d59a147a93764
Mickael Agniel <mika5883@gmail.com>
2024-10-22 16:26:00 +0200

    actualisation dynamique année en cours footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -2,10 +2,12 @@ import Logo from "../../Assets/Logo_blanc.png";
 import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer">
       <img src={Logo} alt="logo kasa" />
-      <p>© 2020 Kasa. All rights reserved</p>
+      <p>© {currentYear} Kasa. All rights reserved</p>
     </div>
   );
 }
