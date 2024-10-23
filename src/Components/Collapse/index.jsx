5e52ed6d26f1e842c8adca7816d02cc04134a0ca
Mickael Agniel <mika5883@gmail.com>
2024-10-23 12:06:34 +0200

    page about: useState + anim ouverture/fermeture collapse

--- src/Components/Collapse/index.jsx
+++ src/Components/Collapse/index.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import ArrowUp from "../../Assets/arrow_back_top.png";
 import "./Collapse.css";
 
 export function Collapse({ title, content }) {
-  {
-    /*useState pour gérer clic sur arrow*/
-  }
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="collapse">
       <div className="collapse-bar">
         <h3>{title}</h3>
-        <img className="arrow-up" src={ArrowUp} alt="Afficher la description" />
+        <img
+          className={`arrow-up ${isOpen ? "true" : ""}`}
+          src={ArrowUp}
+          alt="Afficher la description"
+          onClick={() => setIsOpen(!isOpen)}
+        />
       </div>
-      <span className="collapse-description">{content}</span>
+      <span className="collapse-description">{isOpen && <p>{content}</p>}</span>
     </div>
   );
 }
