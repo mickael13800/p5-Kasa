a302357860f9b6ea1770f08ba859d200164fec90
Mickael Agniel <mika5883@gmail.com>
2024-10-23 15:22:42 +0200

    mise en place overlay sur cards page d'accueil

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -4,6 +4,7 @@ export function Cards({ title, image }) {
   return (
     <div className="cards">
       <img src={image} alt="habitation en location" />
+      <div className="overlay"></div>
       <h3>{title}</h3>
     </div>
   );
